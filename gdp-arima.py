c2d305a825ffab65eb45c373876544c774954182
raihua <raihua.dev@gmail.com>
2023-08-20 00:30:42 +0930

    Change: forecasts to 2050

--- gdp-arima.py
+++ gdp-arima.py
@@ -1,26 +1,33 @@
 from pandas import read_csv
 import pandas as pd
-from pandas import DataFrame
-from statsmodels.tsa.arima.model import ARIMA
 from matplotlib import pyplot
+from statsmodels.tsa.arima.model import ARIMA
+import warnings
+
 
 def parser(x):
     year = int(x)
     return pd.to_datetime(str(year))
 
-series = read_csv('ev-sales.csv', header=0, parse_dates=[0], index_col=0, date_parser=parser)
+# Read the data with the appropriate date format
+series = read_csv('ev-sales.csv', header=0, parse_dates=[0], index_col=0, date_format='%Y')
 
-# fit model
-model = ARIMA(series, order=(5,1,0))
+
+# Suppress specific warnings
+warnings.simplefilter(action='ignore', category=UserWarning)
+
+# Train the ARIMA model using the entire dataset
+model = ARIMA(series, order=(4, 1, 1))
 model_fit = model.fit()
-# summary of fit model
-print(model_fit.summary())
-# line plot of residuals
-residuals = DataFrame(model_fit.resid)
-residuals.plot()
+
+# Forecast for the years 2023 to 2050
+forecast_years = pd.date_range(start='2023-01-01', end='2050-01-01', freq='AS-JAN')
+forecast = model_fit.forecast(steps=len(forecast_years))  # Generate the forecast
+
+# Plot the forecast
+pyplot.plot(series.index, series.values, label='Historical Data')
+pyplot.plot(forecast_years, forecast, color='red', label='Forecast')
+pyplot.xlabel('Time')
+pyplot.ylabel('EVSales')
+pyplot.legend()
 pyplot.show()
-# density plot of residuals
-residuals.plot(kind='kde')
-pyplot.show()
-# summary stats of residuals
-print(residuals.describe())
